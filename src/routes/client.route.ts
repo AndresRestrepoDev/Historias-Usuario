35dbbff09459c895118c3b5381ed3ecd00e025be
AndresRestrepoDev <restreporamirezandresfelipe@gmail.com>
2025-10-22 23:18:20 -0500

    feat: task-3 -> configurar DTO y validaciones centralizadas

--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { ClientController } from "../controllers/client.controller.ts";
+import { validate } from "../middlewares/validate.middleware.ts";
+import { ClientSchema } from "../dtos/client.dto.ts";
 
 const router = Router();
 
 router.get('/', ClientController.getAllClients);
 router.get('/:id', ClientController.getClientById);
-router.post('/', ClientController.createClient);
-router.put('/:id', ClientController.updateClient);
+router.post('/', validate(ClientSchema), ClientController.createClient);
+router.put('/:id', validate(ClientSchema), ClientController.updateClient);
 router.delete('/:id', ClientController.deleteClient);
 
 export default router;
